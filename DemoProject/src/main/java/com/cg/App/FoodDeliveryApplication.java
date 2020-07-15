a258cebc5d5ca72b99612065357405bba5dd5c6c
SpandanaPalakurthy <50132713+SpandanaPalakurthy@users.noreply.github.com>
2020-07-15 16:30:04 +0530

    yes

---
+++ DemoProject/src/main/java/com/cg/App/FoodDeliveryApplication.java
@@ -0,0 +1,17 @@
+package com.cg.App;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryApplication.class, args);
+		
+		System.out.println("Hello !!!!!!!!!!!! \n welcome to Food application system ");
+		
+	}
+
+}
